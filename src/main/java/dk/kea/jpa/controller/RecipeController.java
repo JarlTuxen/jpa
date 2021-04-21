d15039119c8ee2457d8e811506527736cb6f0cd8
jarltuxen <jarl.tuxen@gmail.com>
2021-04-21 14:55:31 +0200

    repository og services - for stort projekt til en undervisningsgang

--- src/main/java/dk/kea/jpa/controller/RecipeController.java
+++ src/main/java/dk/kea/jpa/controller/RecipeController.java
@@ -1,17 +1,26 @@
 package dk.kea.jpa.controller;
 
+import dk.kea.jpa.model.Ingredient;
+import dk.kea.jpa.model.Recipe;
 import dk.kea.jpa.service.RecipeService;
+import dk.kea.jpa.service.IngredientService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RecipeController {
 
     RecipeService recipeService;
+    IngredientService ingredientService;
+
     // constructor injection
-    public RecipeController(RecipeService recipeService ){
+    public RecipeController(RecipeService recipeService, IngredientService ingredientService ){
         this.recipeService = recipeService;
+        this.ingredientService = ingredientService;
     }
 
     @GetMapping("/")
@@ -23,4 +32,35 @@ public class RecipeController {
         return "index";
     }
 
+    @GetMapping("/create")
+    public String create(Model model){
+        Iterable<Ingredient> ingredientList = ingredientService.findAll();
+        model.addAttribute("ingredients", ingredientList);
+        return "createrecipe";
+    }
+
+    @PostMapping("/create")
+    public String create(@ModelAttribute Recipe recipe){
+        recipeService.save(recipe);
+        return "redirect:/";
+    }
+
+    @GetMapping("/update/{id}")
+    public String update(@PathVariable("id") Long id, Model model, Model modelingredient){
+        model.addAttribute("recipe", recipeService.findById(id));
+        modelingredient.addAttribute("ingredients", ingredientService.findAll());
+        return "updateStudent";
+    }
+
+    @PostMapping("/update")
+    public String update(@ModelAttribute Recipe recipe){
+        recipeService.save(recipe);
+        return "redirect:/";
+    }
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable("id") Long id) {
+        recipeService.deleteById(id);
+        return "redirect:/";
+    }
+}
 }
--- src/main/java/dk/kea/jpa/service/RecipeService.java
+++ src/main/java/dk/kea/jpa/service/RecipeService.java
@@ -1,6 +1,10 @@
 package dk.kea.jpa.service;
 
+import dk.kea.jpa.model.Ingredient;
+import dk.kea.jpa.model.Notes;
 import dk.kea.jpa.model.Recipe;
+import dk.kea.jpa.repository.IngredientRepository;
+import dk.kea.jpa.repository.NotesRepository;
 import dk.kea.jpa.repository.RecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +20,12 @@ public class RecipeService {
     @Autowired
     RecipeRepository recipeRepository;
 
+    @Autowired
+    NotesRepository notesRepository;
+
+    @Autowired
+    IngredientRepository ingredientRepository;
+
     public Set<Recipe> findAll(){
         //hent alle opskrifter fre repo med finaAll og returner collection
         Set<Recipe> recipes = new HashSet<>();
@@ -23,6 +33,52 @@ public class RecipeService {
             recipes.add(recipe);
         }
         return recipes;
+    }
+
+    //findById
+    public Recipe findById(long id){
+        //findById giver en optional
+        Optional<Recipe> recipeOptional = recipeRepository.findById(id);
+        //hvis der ikke findes et product på id, kastes der en fejl
+        if (!recipeOptional.isPresent())
+        {
+            throw new RuntimeException("Recipe not found");
+        }
+        //returner product vha. .get()
+        return recipeOptional.get();
+    }
+
+    //create - ny note og ingredient
+    public void create(Recipe recipe){
+        //der skal sættes reference begge veje på notes, så først opretres en ny opskrift
+        Recipe _recipe = new Recipe(r.getDescription(), r.getPrepTime(), r.getCookTime(), r.getServings(), r.getSource(), r.getUrl(), r.getDirections());
+
+        //brug af notes-objekt i r og gem med nye reference itl recipe
+        Notes _notes=r.getNotes();
+        _notes.setRecipe(_recipe);
+        notesRepository.save(_notes);
+
+        //gem de nye ingredients
+        Set<Ingredient> _ingredients = r.getIngredients();
+        for (Ingredient ingredient : _ingredients){
+            ingredient.setRecipe(_recipe);
+            ingredientRepository.save(ingredient);
+        }
+        _recipe.setIngredients(_ingredients);
+
+        //gem recipe
+        recipeRepository.save(_recipe);
+
+    }
+
+    //update
+    public void update(Recipe recipe){
+        recipeRepository.save(recipe);
+    }
+
+    //deleteById
+    public void deleteById(long id){
+        recipeRepository.deleteById(id);
     }
 
 }
